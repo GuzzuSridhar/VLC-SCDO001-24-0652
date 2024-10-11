3d7ab41fea69cc7b4620adce17482546be873b7d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-11 17:45:36 +0530

    add variables

---
+++ basics/variables.py
@@ -0,0 +1,10 @@
+# comments in python
+
+# variable to store Strings (text)
+name = "James Bond"
+city = "Singapore"
+
+print(name)
+print(city)
+
+
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("Hello Python")
+print("Hello VScode")
+print("welcome")
+
