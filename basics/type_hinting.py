400585a705842cb483619195b051014571ad3e48
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-11 18:58:31 +0530

    add variables

---
+++ basics/type_hinting.py
@@ -0,0 +1,5 @@
+# just to indicate to the devloper that a variable is of a type
+age: int = 25
+name: str = "Name"
+price: float = 77.77
+is_valid: bool = True
---
+++ basics/types.py
@@ -0,0 +1,10 @@
+name = "String"
+age = 45
+price = 98.55
+is_valid = True
+
+# determine the type of the variable
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(is_valid))
