fc4a91922e0f5853eb6f1199e3e0a99c39a2d6c4
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-11 19:17:09 +0530

    add print stmt

---
+++ basics/print/print_demo.py
@@ -0,0 +1,18 @@
+name = "Alex"
+age = 35
+
+#  printing individual variables (there is a \n infused towards the end)
+print(name) 
+#  print(name\n) implicitly
+print(age)
+
+#  to print the name and age in one line
+print(name, end=" ")
+print(age)
+
+print(name, end=" - ")
+print(age)
+
+# default functionality
+print(name, end="\n")
+print(age)
