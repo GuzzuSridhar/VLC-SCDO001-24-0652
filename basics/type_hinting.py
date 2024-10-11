ede381dcce1574f670e6a19a1dcc2b279529eb20
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-11 19:00:17 +0530

    add type hinting

--- basics/type_hinting.py
+++ basics/type_hinting.py
@@ -1,5 +1,8 @@
-# just to indicate to the devloper that a variable is of a type
+# just to indicate / hint to the devloper that a variable is of a type
 age: int = 25
 name: str = "Name"
 price: float = 77.77
 is_valid: bool = True
+
+age = "xx"
+print(age)
