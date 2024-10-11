1e4223ddfc1fbefba62c4619a13947cf475003ed
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-11 17:55:32 +0530

    update variables

--- basics/variables.py
+++ basics/variables.py
@@ -1,10 +1,25 @@
 # comments in python
 
 # variable to store Strings (text)
+import dis
+
+
 name = "James Bond"
 city = "Singapore"
 
 print(name)
 print(city)
 
+# variable to store whole number types
+age = 45
+quantity = 10
 
+print(age)
+print(quantity)
+
+# variables for numbers with decimal points
+price = 10.43
+distance = 4.2
+
+print(price)
+print(distance)
