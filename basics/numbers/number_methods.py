4f76fac420635134d096c8723a1b718663e42329
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 13:15:02 +0530

    add round

--- basics/numbers/number_methods.py
+++ basics/numbers/number_methods.py
@@ -1,5 +1,5 @@
 # number methods
-
+import math
 # abs
 # ensures that the output is always positive
 print(f"{abs(-7) = }")
@@ -14,5 +14,23 @@ print(abs(net))
 price = 7.71
 print(int(price))
 
+# float
 qty = 9
 print(float(qty))
+
+# round
+#  Rounds a number to the nearest integer or to a given number of decimal places.
+# rounds to the nearest integer, with .5 rounding to the nearest even integer.
+print(round(4.6))         # Output: 5
+print(round(4.5))         # Output: 4 (rounds to nearest even number)
+print(round(4.567, 2))    # Output: 4.57 (rounds to 2 decimal places)
+
+# ceil
+# Rounds a number up to the nearest integer (always up).
+print(math.ceil(4.1))     # Output: 5
+print(math.ceil(4.9))     # Output: 5
+
+# floor
+# Rounds a number down to the nearest integer (always down).
+print(math.floor(4.1))    # Output: 4
+print(math.floor(4.9))    # Output: 4
