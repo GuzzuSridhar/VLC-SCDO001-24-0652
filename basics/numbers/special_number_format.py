a77a7e14cebfa15b9132f6b5b3f8aacc68544959
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 13:32:11 +0530

    add number bases

---
+++ basics/numbers/special_number_format.py
@@ -0,0 +1,21 @@
+#decimals
+# binary numbers -- base 2 (0 and 1)
+# octal numbers -- base 8 (0 and 7)
+# headecimal numbers -- base 16 ( 0 and 9 , a and f)
+
+binary_num = 0b1010       # Binary (10 in decimal)
+octal_num = 0o12          # Octal (10 in decimal)
+hex_num = 0xA             # Hexadecimal (10 in decimal)
+
+# print the decimal equivalents
+print(binary_num)
+print(octal_num)
+print(hex_num)
+
+# convert decimal to binary
+dec = 42
+print(bin(dec))
+print(oct(dec))
+print(hex(dec))
+
+
