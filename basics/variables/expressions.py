69ff4d83127d6fb55bb9c66cd03ef0cdbb17c62f
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 07:52:42 +0530

    add expressions

---
+++ basics/variables/expressions.py
@@ -0,0 +1,8 @@
+#  how to use variables in expressions
+
+salary = 100
+comm = 10
+print("take home", salary+comm)
+print("annual salary", (salary+comm)*12)
+
+# understand the BODMAS for expression evaluation
