3896de007a24a215e1e8b96a3f732fbd8a7fe78e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 07:14:00 +0530

    add multi vars

---
+++ basics/variables/declare_multiple_var.py
@@ -0,0 +1,27 @@
+# declare multiple variable on single line
+
+# assign the same value to x y and z
+x=y=z=0
+print(x)
+print(y)
+print(z)
+
+# assign different values to a b and c
+a,b,c = 10,"Hello",True
+print(a)
+print(b)
+print(c)
+
+
+var_one = 10
+var_two = 20
+
+print("Var One is: ", var_one) 
+print("Var Two is: ",var_two) 
+
+# Swapping variable values
+var_one,var_two = var_two,var_one
+
+print("Var One is: ", var_one) 
+print("Var Two is: ",var_two) 
+
