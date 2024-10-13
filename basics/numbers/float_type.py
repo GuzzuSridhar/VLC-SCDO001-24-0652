707b7edde19077166c5d42f732d5534e1e5ff371
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 12:43:14 +0530

    add float

---
+++ basics/numbers/float_type.py
@@ -0,0 +1,10 @@
+#  numbers with decimal places (values)
+# example
+price = 77.4
+print(type(price))
+age = 17.5
+count = 1.4
+
+test = 1
+print(type(test))
+print(test)
--- basics/numbers/int_type.py
+++ basics/numbers/int_type.py
@@ -1,4 +1,10 @@
 # integers are whole numbers without decimal places
 # example
 age = 25
+print(type(age))
 salary = 4000
+count = -1
+
+#  in python number types (int and float) are signed
+# signed integer means the variable can hold positive and negative values
+# unsigned integer means the variable can hold only positive  values
