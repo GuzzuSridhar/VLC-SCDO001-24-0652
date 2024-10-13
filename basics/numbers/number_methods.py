0d8f543201e6da23bde4dbeb4c1705ca480d55b8
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 12:49:51 +0530

    add more functions

---
+++ basics/numbers/number_methods.py
@@ -0,0 +1,18 @@
+# number methods
+
+# abs
+# ensures that the output is always positive
+print(f"{abs(-7) = }")
+
+earnings = 1000
+deductions = 250
+# net = earnings- deductions
+net = deductions -earnings 
+print(abs(net))
+
+# int
+price = 7.71
+print(int(price))
+
+qty = 9
+print(float(qty))
