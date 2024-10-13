0941b83a5f8c4d09443c8672b1e57d101138eeb9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 06:48:25 +0530

    add constants

---
+++ basics/variables/constants.py
@@ -0,0 +1,19 @@
+# python does not have an implicit way of imposing the constants
+# declare a constant (name the variable in caps)
+# the declaration does not enforce the variable to be a constant
+#  the value can still be changed
+# use ENUMS to enforce constants
+
+PI = 3.14
+MAX_CONNECTIONS = 10
+FINAL_PRICE = 100
+RATE_OF_INTEREST = 4
+
+print(PI)
+print(MAX_CONNECTIONS)
+print(FINAL_PRICE)
+print(RATE_OF_INTEREST)
+
+
+
+
