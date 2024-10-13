02d6758cf20b0c577bd98d7eba0a22d35dc6db6c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 12:32:24 +0530

    use fstring align

--- basics/strings/fstrings.py
+++ basics/strings/fstrings.py
@@ -19,3 +19,22 @@ print("Name: ", name)
 print(f"{age = }")
 print(f"{age+10 = }")
 print(f"{name = }")
+
+# Aligning strings without using fstring
+print("Name".center(30),"Age".center(5),"City".center(15))
+print("Alex".center(30),"45".center(5),"Kl".center(15))
+print("David".center(30),"35".center(5),"Singapore".center(15))
+
+# Aligning strings  using fstring
+print(f"{'name':<30} | {'age':<5} | {'City':<20} |")
+print(f"{'name':>30} {'age':>5} {'City':>20}")
+print(f"{'name':^30} {'age':^5} {'City':^20}")
+
+# using custom character to fill spaces
+print(f"{'name':-^30} {'age':-^5} {'City':-^20}")
+print(f"{'name':*^30} {'age':*^5} {'City':*^20}")
+
+
+
+
+
