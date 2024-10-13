9730303259c936886ac743a42b4b054d0fef0e52
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-13 13:49:47 +0530

    add casting

---
+++ basics/casting/float_cast.py
@@ -0,0 +1,18 @@
+# float(x): Converts x to a float. It works with integers and strings containing numeric values.
+# If x is a string, it must represent a valid number, or it raises an error.
+
+# int to float
+float_val = float(5)
+print(float_val)
+
+# str to float
+float_val = float("6.77")
+print(float_val)
+
+# boolean to float
+float_val = float(True)
+print(float_val)
+float_val = float(False)
+print(float_val)
+
+
---
+++ basics/casting/string_cast.py
@@ -0,0 +1,16 @@
+# str(x): Converts x to a string.
+# This works on almost any type of object, converting numbers, lists, and other types into their string representations.
+
+# float to String
+str_val = str(5.5)
+print(str_val)
+
+# int to String
+str_val = str(58)
+print(str_val)
+
+# boolean to String
+str_val = str(True)
+print(str_val)
+str_val = str(False)
+print(str_val)
