c6dd05694a5296d88ce646ae39c788d62ffa34e9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-17 18:09:51 +0530

    add comparision operators

---
+++ basics/operators/comparison_operators.py
@@ -0,0 +1,57 @@
+#  used to compare  values or variables
+# the result of applying a comparison operator is always boolean (true or false)
+
+#  types of comparison operators are
+# == -- equality check (used with numbers and strings)
+# != -- inequality check (used with numbers and strings)
+# > -- greater than (used with numbers and strings)
+# >= -- greater than equals (used with numbers and strings)
+# < -- lesser than (used with numbers and strings)
+# <= -- lesser than equals (used with numbers and strings)
+
+# ------------equals operator------------------
+# can be used with strings and numbers
+
+# with strings (case sensitive comparision)
+# use string methods to do a case insensitive comparision
+str1 = "Hello"
+str2 = "hello"
+print(str1 == str2)
+print(str1.casefold() == str2.casefold())
+
+# with numbers
+x = 10
+y = 20
+print (x == y)
+#  print (f"{x == y}")
+
+# ------------inequals operator------------------
+# can be used with  numbers and strings
+# with strings (case sensitive comparision)
+print (x != y)
+str1 = "Hello"
+str2 = "Hello"
+print(str1 != str2)
+print(str1 == str2)
+# with numbers
+print (x != y)
+
+# ------------greater operator------------------
+# can be used with  numbers and strings
+# compare lexicographically
+str1 = "ABC"
+str2 = "DEF"
+print(str1 > str2)
+
+print(x > y)
+print(y > x)
+
+
+
+
+
+
+
+
+
+
