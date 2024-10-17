89aa7284882d8624fed58e0d5c846e8e6cd0f64b
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-17 17:09:03 +0530

    add plus equals

---
+++ basics/operators/assignment_operators.py
@@ -0,0 +1,33 @@
+# used to assing value to a variable
+# Example
+# age = 45
+# in the case above the assignment operator was "="
+
+# list of assignment operators
+# equals -- = (works with all types)
+# plus equals -- += (works with numbers and string)
+# minus equals -- -= (works with numbers only)
+# multiply equals -- *= (works with numbers only)
+# divide equals -- /= (works with numbers only)
+# modulo equals -- %= (works with numbers only)
+
+# equals
+age = 10
+name = "Alex"
+price = 99.7
+is_valid = True
+
+# plus operator
+count = 10
+# add one to count (11)
+count += 1 
+print(count)
+# add 5 to count
+count += 5
+print(count)
+
+# with strings
+msg = "Hello! "
+# using the += on strings
+msg += "Alex"
+print(msg)
