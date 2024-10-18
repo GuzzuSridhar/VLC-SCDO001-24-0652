27c7b28439c5839a87f9a9bf9ab71b55f9d98be8
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-18 19:29:28 +0530

    add ternary

---
+++ control_flow/conditions/ternary_operator/simple_ternary.py
@@ -0,0 +1,9 @@
+#  is a shortcut to write if and else
+
+# age = 18
+
+age= int(input("Enter your age: "))
+print("Major" if age>=18 else "Minor")
+
+# status = "Major" if age >=18 else "Minor"
+# print(status)
