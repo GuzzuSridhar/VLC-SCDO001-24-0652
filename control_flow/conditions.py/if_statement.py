dc343b989f6ce12a5c265561519735f30d5199a4
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-18 17:07:33 +0530

    add if statement

---
+++ control_flow/conditions.py/if_statement.py
@@ -0,0 +1,17 @@
+# conditionally executing statements
+# using the if statement
+# indententation levels are important
+
+# season = "winter"
+season = input("Enter the season: ")
+
+if season == "winter":
+    print("wear woolen clothes")
+    print("stay indoors")
+
+if season == "summer":
+    print("wear cotton clothes")
+    print("use air conditioner")
+
+if season == "rainy":
+    print("carry an umbrella")
