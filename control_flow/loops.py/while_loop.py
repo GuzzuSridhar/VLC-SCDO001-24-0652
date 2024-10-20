39a00ac1f6c55ef963eb2b2daf61732e375352e1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-20 08:08:07 +0530

    add loops

---
+++ control_flow/loops.py/while_loop.py
@@ -0,0 +1,33 @@
+
+#  simple loop
+
+# declare a loop variable
+count = 0
+
+# exit condition (count = 10)
+while count < 10:
+    # increment the loop variable
+    count += 1
+    print("Count", count)
+# print(f"{count = }") 
+
+print("--------------------------")
+
+# sample 2 (reverse)
+while count > 0:
+    print(f"{count = }")
+    count -= 1
+
+
+# loops on strings
+
+#  access the characters in a string
+
+# name = "Alex"
+name = input("Enter your name: ")
+str_idx = 0
+while str_idx < len(name):
+    print(name[str_idx], end="")
+    str_idx += 1
+
+
