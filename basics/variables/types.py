25792b52a3b8e8563493ef23c9eca366fdd6589c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-20 06:58:42 +0530

    add none type check

--- basics/variables/types.py
+++ basics/variables/types.py
@@ -2,9 +2,12 @@ name = "String"
 age = 45
 price = 98.55
 is_valid = True
+city = None
+
 
 # determine the type of the variable
 print(type(name))
 print(type(age))
 print(type(price))
 print(type(is_valid))
+print(type(city))
---
+++ control_flow/conditions/check_for_none.py
@@ -0,0 +1,11 @@
+city = None
+print(type(city))
+
+if city is None:
+    print("no value is assigned")
+
+city = "SG"
+print(type(city))
+
+if city is not None:
+    print("value has been assigned")
