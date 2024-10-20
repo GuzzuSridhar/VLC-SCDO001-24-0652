86c33885f48a4a37662f171b20929d8843eeec46
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-20 06:43:21 +0530

    add pass statement

---
+++ control_flow/pass_statement.py
@@ -0,0 +1,8 @@
+# the pass statement is a null operation. It serves as a placeholder,
+# allows defining a syntactically correct code block without any actual implementation.
+
+age = 20
+if age>= 18:
+    pass
+else:
+    pass
