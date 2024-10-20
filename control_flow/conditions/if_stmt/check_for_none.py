d64b976bd739e836ebe61c3bf137a54c7621f8e6
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-20 07:11:06 +0530

    change folder

--- control_flow/conditions/if_stmt/check_for_none.py
+++ control_flow/conditions/if_stmt/check_for_none.py
@@ -4,8 +4,8 @@ print(type(city))
 if city is None:
     print("no value is assigned")
 
-city = "SG"
+city = input("Enter the city:")
 print(type(city))
-
 if city is not None:
     print("value has been assigned")
+
