10ba3d3dbcbc7ddfcd8c173b72ad67fb42b2a9cf
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-20 12:23:42 +0530

    add lists

---
+++ collections/list/simple_list.py
@@ -0,0 +1,17 @@
+# Homogenous list
+
+#  list of numbers
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+# list of Strings
+names = ['Alex',"David","Jane", 'Sara','James']
+
+# empty list
+emp_list = []
+
+# Hetrogenous List
+mixed_list = [1,2,"Hello",True,10.88]
+
+# nested lists
+nest_list = [[1,2][3,4][5,6]]
+
