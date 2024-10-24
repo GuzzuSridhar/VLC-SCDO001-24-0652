307f7fb5b41c458278fb13adf1f0c3d4e3070c58
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-24 18:53:20 +0530

    add __name__

---
+++ functions/dunder_variable.py/call.py
@@ -0,0 +1,3 @@
+import greet
+
+print(greet.greet())
---
+++ functions/dunder_variable.py/greet.py
@@ -0,0 +1,15 @@
+# greet.py
+
+def greet():
+    return "Testing function calls"
+
+
+
+if __name__ =="__main__":
+    print("Test print statement")
+    print(greet())
+
+# if __name__ =="__main__":
+#     print(f"{__name__ = }")
+# else:
+#     print(f"{__name__ = }")
