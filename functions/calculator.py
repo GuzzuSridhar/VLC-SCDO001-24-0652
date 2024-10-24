2b0f7c3017e98bc6d7b365a99f0037039d0cf635
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-24 17:43:01 +0530

    add functions

---
+++ functions/calculator.py
@@ -0,0 +1,15 @@
+# functions that take parameters
+
+def add(x,y):
+    print(x+y)
+
+def subtract(a,b):
+    print(a-b)
+
+#  calling the functions
+
+add(10,20)
+add("Hello"," World")
+
+subtract(20,10)
+
---
+++ functions/function_definitions.py
@@ -0,0 +1,13 @@
+# simple function definition
+
+def greet():
+    print("Welcome")
+
+# call a function
+greet()
+
+# using the pass key word
+def get_name():
+    pass
+
+get_name()
