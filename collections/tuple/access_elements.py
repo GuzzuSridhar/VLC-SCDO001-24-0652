ad2d240709f2474b03825ba9a6dd963b5a51104e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-24 16:56:18 +0530

    add tuple methods

---
+++ collections/tuple/access_elements.py
@@ -0,0 +1,14 @@
+my_tuple = (1,2,3,4,5,6)
+
+# access the 1st element
+print(my_tuple[0])
+print(my_tuple[-1])
+
+# slicing
+print(my_tuple[1:3])
+
+# access all elements
+print(my_tuple[:])
+print(my_tuple)
+
+
