6b3067f5b5761c89b0930d458ae0320e8725c429
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-24 17:11:14 +0530

    add dict

---
+++ collections/dictionary/simple_dict.py
@@ -0,0 +1,22 @@
+#  collection with a key:value pairs
+# hetrogenous values allowed
+
+employee_info = {
+    "name" :"Alice",
+    "age": 30,
+    "department" : "Sales",
+    "city": "Singapore",
+    "hobbies": ["reading","running"],
+    "address":{
+        "street": "Some Street",
+        "state" : "Some State",
+        "zip" : '0000'
+    }
+}
+
+#  keys are
+
+# name
+# age
+# department
+# city
